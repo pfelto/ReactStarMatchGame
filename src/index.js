93ac604ae89948f6a7367d43dba78c100661e43c
pfelto <paul.r.felton@gmail.com>
2021-11-04 11:40:01 -0400

    changed the static markup template to be dynamic and extracted playButton component out of the original StarMatch component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//PlayButton Component
+const PlayButton = ({number}) => {
+  return(<button key={number} onClick={() => console.log(number)}className="number">{number}</button>)
+}
 
 // STAR MATCH - Starting Template
 
 const StarMatch = () => {
+  const [stars, setStars] = React.useState(utils.random(1,9));
   return (
     <div className="game">
       <div className="help">
@@ -13,32 +18,23 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
+          {utils.range(1,stars).map((starId) => 
+            <div key={starId} className='star'></div>
+        )}
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+        {utils.range(1,9).map((number) => 
+            <PlayButton key={number} number={number} />
+        )}
+          
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
     </div>
   );
 };
+
+
 
 // Color Theme
 const colors = {
